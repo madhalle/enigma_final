02ac8767b75976aa18b97420ce94834d53679077
Mad Hallé <mad.halle@icloud.com>
2020-04-19 15:08:21 -0600

    testing shift attributes

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -11,8 +11,15 @@ class ShiftTest < Minitest::Test
     @key = Key.new
     @offset = Offset.new
   end
-  
+
   def test_it_exists
     assert_instance_of Shift, @shift
+  end
+
+  def test_it_has_attributes
+    assert_equal 0, @shift.a_shift
+    assert_equal 0, @shift.b_shift
+    assert_equal 0, @shift.c_shift
+    assert_equal 0, @shift.d_shift
   end
 end
