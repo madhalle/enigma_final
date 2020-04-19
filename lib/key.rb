e2bd47b586d9fa159dc4fbccff465b0df9569a70
Mad Hallé <mad.halle@icloud.com>
2020-04-19 13:14:05 -0600

    testing key class contains separate kets

--- lib/key.rb
+++ lib/key.rb
@@ -1,6 +1,10 @@
 class Key
-  attr_reader :key
+  attr_reader :key, :a_key, :b_key, :c_key, :d_key
   def initialize
     @key = key
+    @a_key = a_key
+    @b_key = b_key
+    @c_key = c_key
+    @d_key = d_key
   end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -6,8 +6,16 @@ class KeyTest < Minitest::Test
   def setup
     @key = Key.new
   end
-  
+
   def test_it_exists
     assert_instance_of Key, @key
+  end
+
+  def test_it_has_keys
+    assert_nil @key.key
+    assert_nil @key.a_key
+    assert_nil @key.b_key
+    assert_nil @key.c_key
+    assert_nil @key.d_key
   end
 end
