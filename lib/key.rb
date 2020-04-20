b9b706eb8306b5134cbdf5d8d8e14cfa3627fab4
Mad Hallé <mad.halle@icloud.com>
2020-04-20 12:28:15 -0600

    key attributes tested and running

--- lib/key.rb
+++ lib/key.rb
@@ -1,11 +1,8 @@
 class Key
-  attr_reader :key, :a_key, :b_key, :c_key, :d_key
+  attr_reader :key, :keys
   def initialize
     @key = key
-    @a_key = a_key
-    @b_key = b_key
-    @c_key = c_key
-    @d_key = d_key
+    @keys = Hash.new
   end
 
   def generate
