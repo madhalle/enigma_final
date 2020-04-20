2e538a7321ac1709e5ed0d08d36cd6fd28627a8e
Mad Hallé <mad.halle@icloud.com>
2020-04-20 12:27:37 -0600

    refactoring key class, testing attributes

--- test/key_test.rb
+++ test/key_test.rb
@@ -11,27 +11,24 @@ class KeyTest < Minitest::Test
     assert_instance_of Key, @key
   end
 
-  def test_it_has_keys
+  def test_it_has_attributes
     assert_nil @key.key
-    assert_nil @key.a_key
-    assert_nil @key.b_key
-    assert_nil @key.c_key
-    assert_nil @key.d_key
+    assert_equal ({}), @key.keys
   end
 
-  def test_generate_key
-    @key.stubs(:rand).returns("5188")
-    assert_equal "05188", @key.generate
-  end
-
-  def test_split_method
-    @key.stubs(:rand).returns("5188")
-    assert_equal "05188", @key.generate
-    @key.split
-    assert_equal 05, @key.a_key
-    assert_equal 51, @key.b_key
-    assert_equal 18, @key.c_key
-    assert_equal 88, @key.d_key
-  end
+  # def test_generate_key
+  #   @key.stubs(:rand).returns("5188")
+  #   assert_equal "05188", @key.generate
+  # end
+  #
+  # def test_split_method
+  #   @key.stubs(:rand).returns("5188")
+  #   assert_equal "05188", @key.generate
+  #   @key.split
+  #   assert_equal 05, @key.a_key
+  #   assert_equal 51, @key.b_key
+  #   assert_equal 18, @key.c_key
+  #   assert_equal 88, @key.d_key
+  # end
 
 end
