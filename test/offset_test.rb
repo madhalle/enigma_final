5647c8e143bf72c1dd3846bad6ab7b07cc5dad8a
Mad Hallé <mad.halle@icloud.com>
2020-04-20 12:36:27 -0600

    refactoring offset test attributes

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -13,18 +13,23 @@ class OffsetTest < Minitest::Test
     assert_instance_of Offset, @offset
   end
 
-  def test_offset_date
-    #stubs use case here?
-    todays_date = '190420'
-    # todays_date = Time.now.strftime("%d%m%y")
-    assert_equal todays_date, @offset.date
+  def test_it_has_attributes
+    assert_nil @offset_date
+    assert_equal ({}), @offsets
   end
 
-  def test_create_offsets
-    @offset.generate
-    assert_equal 6, @offset.a_offset
-    assert_equal 4, @offset.b_offset
-    assert_equal 0, @offset.c_offset
-    assert_equal 0, @offset.d_offset
-  end
+  # def test_offset_date
+  #   #stubs use case here?
+  #   todays_date = '190420'
+  #   # todays_date = Time.now.strftime("%d%m%y")
+  #   assert_equal todays_date, @offset.date
+  # end
+  #
+  # def test_create_offsets
+  #   @offset.generate
+  #   assert_equal 6, @offset.a_offset
+  #   assert_equal 4, @offset.b_offset
+  #   assert_equal 0, @offset.c_offset
+  #   assert_equal 0, @offset.d_offset
+  # end
 end
