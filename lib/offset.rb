41a4025623cdd9991f3756aa213a4ce2821dd798
Mad Hallé <mad.halle@icloud.com>
2020-04-20 12:40:22 -0600

    generate method tested and running, now created a hash of offsets

--- lib/offset.rb
+++ lib/offset.rb
@@ -10,12 +10,14 @@ class Offset
   end
 
   def generate
-    date_num = date.to_i
+    create_date
+    date_num = @offset_date
     offset = ((date_num.to_i*date_num.to_i).to_s[-4..-1])
-    @a_offset = offset[0].to_i
-    @b_offset = offset[1].to_i
-    @c_offset = offset[2].to_i
-    @d_offset = offset[3].to_i
+    @offsets[:A] = offset[0].to_i
+    @offsets[:B] = offset[1].to_i
+    @offsets[:C] = offset[2].to_i
+    @offsets[:D] = offset[3].to_i
+    @offsets
   end
 
 end
