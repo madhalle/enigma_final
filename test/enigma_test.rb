2f710f1545a5c875e76eb7577e59f65694d038f5
Mad Hallé <mad.halle@icloud.com>
2020-04-20 21:41:10 -0600

    testing enigma attributes

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,4 +13,17 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_has_attributes
+    assert_equal "Hello world.", @enigma.message
+    assert_instance_of Hash, @enigma.key
+    assert_equal 5 || 4, @enigma.key[:key].length
+
+
+    expected2 = {:offsets=>{:A=>6, :B=>4, :C=>0, :D=>0}, :date=>"200420"}
+    assert_equal expected2, @enigma.date
+    assert_instance_of Hash, @enigma.date
+
+    assert_equal [], @enigma.alphabet
+  end
+
 end
